546d4ca3d4b240d540f169d6bdd1fd1321b95f9c
configtest <configtest@gmail.com>
2025-03-16 14:23:41 +0000

    Ajour des lorems pour notre projet

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ function App() {
     <div className="App">
       <h1>Mon Projet React</h1>
       <p>Bienvenue !</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat inventore tempora esse nemo? Voluptate tempore nemo voluptates neque deleniti facilis molestias, eligendi libero sint debitis, id vel adipisci unde nesciunt!
+
+      </p>
+      <button>
+        En savoir plus
+      </button>
     </div>
   );
 }
